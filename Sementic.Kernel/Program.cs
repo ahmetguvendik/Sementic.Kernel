0d723de9ed8c47e5a7a1b57cf6aeb5228513a0c8
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2025-05-17 12:44:17 +0300

    Initial commit

---
+++ Sementic.Kernel/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder();
+builder.AddOllamaChatCompletion("deepseek-r1:latest", new Uri("http://localhost:11434"));
+builder.Services.AddScoped<HttpClient>();
+
+var kernel = builder.Build();
+
+while (true)
+{
+    Console.WriteLine("> ");
+    var line = Console.ReadLine();
+    var response = await kernel.InvokePromptAsync(line);    
+    Console.WriteLine(response);
+}
---
+++ SementicKernel.PromptTemplate/Program.cs
@@ -0,0 +1,20 @@
+﻿using Codeblaze.SemanticKernel.Connectors.Ollama;
+using Microsoft.SemanticKernel;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = Kernel.CreateBuilder()
+    .AddOllamaChatCompletion("deepseek-r1:latest", "http://localhost:11434");
+
+builder.Services.AddScoped<HttpClient>();
+
+var kernel = builder.Build();
+
+var promptTemplate = "Yandaki işlemi hesapla: {{$input}}";
+var function = kernel.CreateFunctionFromPrompt(promptTemplate);
+Console.WriteLine("> ");
+var line = Console.ReadLine();
+var arguments = new KernelArguments { ["input"] = line };
+var result = await function.InvokeAsync(kernel, arguments);
+Console.WriteLine(result);
+
+Console.Read();
